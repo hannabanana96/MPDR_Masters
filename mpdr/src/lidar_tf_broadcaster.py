f23767a9e23bcd6e5b832b0e1eab7dd591021dc5
hannabanana96 <39104484+hannabanana96@users.noreply.github.com>
2021-06-10 13:07:04 -0700

    Update lidar_tf_broadcaster.py

--- mpdr/src/lidar_tf_broadcaster.py
+++ mpdr/src/lidar_tf_broadcaster.py
@@ -9,21 +9,6 @@ def lidar_callback(msg):
     lidar_br = tf.TransformBroadcaster()
    
     current_time = rospy.Time.now()
-    """
-    lidar_quat = tf.transformations.quaternion_from_euler(0, 0, 0)
-    lidar_trans = TransformStamped()
-    lidar_trans.header.stamp = current_time
-    lidar_trans.parent = "base_link"
-    lidar_trans.child = "lidar"
-    lidar_trans.time = current_time
-
-    lidar_trans.transform.translation.x = 0.0025
-    lidar_trans.transform.translation.y = 0
-    lidar_trans.transform.translation.z = 0.1905
-    lidar_trans.transform.rotation = lidar_quat
-
-    lidar_br.sendTransform(lidar_trans)
-    """
 
     lidar_br.sendTransform(
         #(-0.3575, 0, 0.1905),		# Lidars xyz displacement from base_link
